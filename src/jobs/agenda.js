a252736f433d4e0023bff3476e04608bbd1d1b9b
Mahmud <ajaomahmud@gmail.com>
2020-07-06 16:08:31 +0100

    feat: added agenda.js dependency

---
+++ src/jobs/agenda.js
@@ -0,0 +1,19 @@
+const Agenda = require('agenda');
+// const email = require('./email');
+
+const connectionOpts = { db: { address: process.env.TEST_DATABASE_URL, collection: 'agendaJobs' } };
+
+const agenda = new Agenda(connectionOpts);
+
+// const jobTypes = process.env.JOB_TYPES ? process.env.JOB_TYPES.split(',') : [];
+const jobTypes = ['email'];
+
+jobTypes.forEach(type => {
+  require(`./${type}`)(agenda);
+});
+
+if (jobTypes.length) {
+  agenda.on('ready', async () => await agenda.start()); // Returns a promise, which should be handled appropriately
+}
+
+module.exports = agenda;
